ee13e68af0589631800b94f75bb78c423b902ba1
extwiii <bcagiran@hotmail.com>
2017-02-22 05:14:25 +0000

    update key of weatherapi

--- weather/index.js
+++ weather/index.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
     var lat, lon;
-    var api = "https://api.openweathermap.org/data/2.5/weather?";
+    var api = "http://api.openweathermap.org/data/2.5/weather?";
     var units = "&units=metric";
-    var appid = "&APPID=c0ef54bd8cb70c355dc7810f8ed32926";
+    var appid = "&APPID=18bdc68210824e61d3df69e496c54c5e";
     var isFahrenheit = false;
 
     $.getJSON('https://ipinfo.io', function(data) {
+
         var res = data.loc.split(",");
         lat = res[0];
         lon = res[1];
 
-        var link = api + "lat=" + lat + "&lon=" + lon + units + appid;
+        var link =  api + "lat=" + lat + "&lon=" + lon + units + appid ;
+        console.log(link)
 
-        $.getJSON(link, function(weather) {
+        $.get(link, function(weather) {
+          console.log(123)
             $('.city').html(weather.name + " , " + weather.sys.country);
             $('.icon').attr('src', 'https://openweathermap.org/img/w/' +
                 weather.weather[0].icon + '.png');
